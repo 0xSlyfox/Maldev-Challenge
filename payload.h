df0d0824e51df3eb31ece45962d2102920b89405
slyf0x <106607439+0xSlyfox@users.noreply.github.com>
2024-02-22 10:29:42 -0700

    Update payload.h

--- payload.h
+++ payload.h
@@ -5,6 +5,6 @@
 #include <Windows.h>
 
 BOOL FetchFileFromURLW(IN LPCWSTR szFileDownloadUrl, OUT PBYTE* ppFileBuffer, OUT PDWORD pdwFileSize);
-BOOL HijackTargetThread(IN HANDLE hThread, IN PVOID pStartAddress);
+BOOL ExecuteShellcodeInLocalProcess(IN PBYTE pShellcodeAddress, IN SIZE_T sShellcodeSize, OUT PBYTE* ppInjectionAddress, OUT OPTIONAL HANDLE* phThread);
 
 #endif
